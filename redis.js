69dc9307e65d8acf11bf65efea558870b9189f8e
Yoann Cribier <yoann_cribier@hotmail.com>
2017-11-12 14:35:14 +0000

    redis - log on events

--- redis.js
+++ redis.js
@@ -2,5 +2,8 @@ const redis = require('redis')
 const { lazyProxy } = require('4k')
 const client = redis.createClient({ password: process.env.REDIS_PASSWORD })
 
+const events = ['connect', 'reconnecting', 'ready', 'end', 'error', 'warning']
+  .forEach(e => client.on(e, p => console.info(`[Redis] ${e}`, p || '')))
+
 module.exports = lazyProxy(key => (...args) => new Promise((s, f) =>
   client[key](...args, (e, r) => e ? f(e) : s(r))))
