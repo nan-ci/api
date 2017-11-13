7400108586d6516da118de0105a09c06ff6fa557
Yoann Cribier <yoann_cribier@hotmail.com>
2017-11-13 12:28:15 +0000

    game01: added main file with 'start' & 'next' methods (+ tests file)

---
+++ game01/game01.js
@@ -0,0 +1,66 @@
+const db = require('../redis')
+const { solve } = require('./solver')
+
+// init -> set game01-USER_X EXP 2Hours
+
+//  start -> ( EXISTS(game01:user) ? GET(game01:user) > GET(lvl) : SET(game01:user, lvl0) ) > send(lvl0)
+//   lvl0 <-
+
+// next#1 -> answer > GET(game01:user) > GET(lvl) > solve(lvl, answer) ? INC(lvl) > send(lvl) : false
+//   lvl1 <-
+
+// user01
+// lvl0: {start: 100, end: 214}
+// lvl1: {start: 100, end: 214}
+// lvl..
+
+const levels = [
+  require('./levels/level1'),
+  require('./levels/level2'),
+  require('./levels/level2b'),
+]
+
+const formatUserKey = userId => `game01:user:${userId}`
+
+const start = ({ session }) => new Promise(s => {
+  const userId = session.id
+  const key = formatUserKey(userId)
+
+  db.exists(key)
+    .then(exists => {
+      if (exists) {
+        db.get(key)
+          .then(JSON.parse)
+            .then(user => s(levels[user.currentLevelId]))
+      } else {
+        const newUser = { currentLevelId: 0 }
+
+        db.set(key, JSON.stringify(newUser))
+        s(levels[0])
+      }
+    })
+})
+
+const nextLevel = userId => new Promise((s, f) => {
+  const key = formatUserKey(userId)
+
+  db.get(key)
+    .then(JSON.parse)
+      .then(user => {
+        const nextLevelId = ++user.currentLevelId
+
+        db.set(key, JSON.stringify(user))
+        s(levels[nextLevelId])
+      })
+      .catch(err => f(err))
+})
+
+const next = ({ answer, session }) =>
+  solve(level, answer) && nextLevel(session.id)
+
+module.exports = {
+  start,
+  next,
+  nextLevel,
+  formatUserKey,
+}
---
+++ game01/game01.test.js
@@ -0,0 +1,55 @@
+const test = require('./tester')
+const { start, next, nextLevel, formatUserKey } = require('./game01')
+
+const {
+  NO, FW, TL, TR, P1, P2, P3, F0, F1, F2, C1, C2, C3,
+  allInstructions,
+  getPossibleInstructions
+} = require('./instructions')
+
+const db = require('../redis')
+
+const levels = [
+  require('./levels/level1'),
+  require('./levels/level2'),
+  require('./levels/level2b'),
+]
+
+const session = { id: '1234.test' }
+const key = formatUserKey(session.id)
+
+test('# method: start - step 0', [
+  t => db.exists(key)
+    .then(reply => t.equal(reply, 0, `redis entry [${key}] should not exists`)),
+])
+
+test('# method: start - step 1', [
+  t => start({session})
+    .then(lvl => t.deepEqual(lvl, levels[0], `entry doesn't exists, level returned should be 1st level`), t.fail)
+])
+
+test('# method: start - step 2', [
+  t => db.exists(key)
+    .then(reply => t.equal(reply, 1, `redis entry [${key}] should exists`))
+])
+
+test('# method: start - step 3', [
+  t => start({session})
+    .then(lvl => t.deepEqual(lvl, levels[0], `entry exists, level returned should be user current level`), t.fail)
+])
+
+test('# method: start - step 4', [
+  t => db.del(key)
+    .then(reply => t.equal(reply, 1, `redis entry [${key}] should be deleted`))
+])
+
+
+test('# method: nextLevel - step 0', [
+  t => start({session})
+    .then(lvl => t.deepEqual(lvl, levels[0], `init gamel`), t.fail)
+])
+
+test('# method: nextLevel - step 1', [
+  t => nextLevel(session.id)
+    .then(lvl => t.deepEqual(lvl, levels[1], `next level returned should be 2nd level`), t.fail)
+])
