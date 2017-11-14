1872b0278668f09e6132eb217bd25a479aeca4d9
Yoann Cribier <yoann_cribier@hotmail.com>
2017-11-14 18:25:40 +0000

    redis: promisify functions & compute props

--- redis.js
+++ redis.js
@@ -3,7 +3,15 @@ const { lazyProxy } = require('4k')
 const client = redis.createClient({ password: process.env.REDIS_PASSWORD })
 
 const events = ['connect', 'reconnecting', 'ready', 'end', 'error', 'warning']
-  .forEach(e => client.on(e, p => console.info(`[Redis] ${e}`, p || '')))
+  .forEach(e => client.on(e, err => console.info(`[Redis] ${e}`, err || '')))
 
-module.exports = lazyProxy(key => (...args) => new Promise((s, f) =>
-  client[key](...args, (e, r) => e ? f(e) : s(r))))
+const db = module.exports = lazyProxy((key, src) => {
+  if (typeof client[key] === 'function') {
+    // promisify functions
+    return src[key] = (...args) => new Promise((s, f) =>
+      client[key](...args, (e, r) => e ? f(e) : s(r)))
+  }
+  // define property for subsequent acces
+  Object.defineProperty(src, key, { get: () => client[key] })
+  return client[key]
+})
