d6a06ba4de3c134d1ac4562205ea0ba6e91fe68b
Yoann Cribier <yoann_cribier@hotmail.com>
2017-12-09 16:17:58 +0000

    cleaned up

--- handlers/users.js
+++ handlers/users.js
@@ -1,20 +1,8 @@
 const db = require('../redis')
 
-const setLocation = ({ user, location }) => {
-  return db.hset('locations', location, user)
-}
+const setLocation = ({ user, location }) => db.hset('locations', location, user)
 
-const getLocations = () => {
-  return db.hgetall('locations')
-    .then(locations => {
-      // error handling
-      console.log('locations:', locations)
-
-      return locations
-    })
-}
-
-getLocations()
+const getLocations = () => db.hgetall('locations')
 
 module.exports = {
   setLocation,
